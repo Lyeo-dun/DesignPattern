de106763b5d4d74f32919ba90abf035832168280
hjd0830 <hjd0830@gmail.com>
2021-08-11 11:01:59 +0900

    응용하기 v0.1

--- Objectpool/main.cpp
+++ Objectpool/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <list>
+#include <string>
 
 using namespace std;
 
@@ -12,28 +13,68 @@ void Safe_Delete(T& _obj)
 }
 #define SAFE_DELETE(type) Safe_Delete(type)
 
-struct Object
+struct Vector3
 {
-	int Key;
-	int Value;
+	float x, y, z;
 
-	Object() : Key(0), Value(0) {}
-	Object(int _Key, int _Value) : Key(_Key), Value(_Value) {}
+	Vector3() {}
+	Vector3(float _x, float _y, float _z = 0) : x(_x), y(_y), z(_z) {}
+};
+struct Transform
+{
+	Vector3 Position;
+	Vector3 Scale;
+};
+class Object
+{
+private:
+	string Texture;
+	Transform Transinfo;
+public:
+	void Initialize()
+	{
+		Texture = "-->";
+		Transinfo.Position = Vector3(2, 15);
+		Transinfo.Scale = Vector3(Texture.length(), 1);
+	}
+	int Update()
+	{
+		Transinfo.Position.x += 2.0f;
+
+		if (Transinfo.Position.x >= (120 - Transinfo.Scale.x))
+		{
+			return 1;
+		}
+		return 0;
+	}
+	void Output()
+	{
+		COORD Pos = { Transinfo.Position.x, Transinfo.Position.y };
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+
+		cout << Texture << endl;
+	}
+
+public:
 	~Object() {}
 };
 
 bool check = false;
 int Count = 0;
 
+void Output(float _x, float _y, string _str);
+
 int main(void)
 {
-	//배열은 리스트, 삭제는 #define safe delete
+	//총알이 벽에 부딪혔을 때 삭제하기!
 	list<Object*> EnableList; //실제 사용한 오브젝트 리스트(랜더링 될 오브젝트 리스트)
 	list<Object*> DesableList; //사용을 마친 오브젝트 리스트
 
 	while (true)
 	{
 		system("cls");
+
+		Output(2, 15, "◎");
 
 		check = false;
 
@@ -49,14 +90,16 @@ int main(void)
 				//없다면 새로운 오브젝트를 만든다
 				for (int i = 0; i < 5; ++i)
 				{
-					DesableList.push_back(new Object(Count++, 0));
+					DesableList.push_back(new Object);
 				}
 			}
 
 			//추가된 오브젝트 하나 선택
 			list<Object*>::iterator iter = DesableList.begin();
+			
 			//초기화
-			(*iter)->Value = 0;
+			(*iter)->Initialize();
+
 			//사용할 리스트 추가
 			EnableList.push_back((*iter));
 			//현재 리스트에서는 삭제
@@ -65,14 +108,12 @@ int main(void)
 		}
 
 		//Render
-		cout << "EnableList" << endl;
 		for (list<Object*>::iterator iter = EnableList.begin(); iter != EnableList.end();/*증감문 따로 제어*/)
 		{
-			(*iter)->Value++;
-			cout << "Key: " << (*iter)->Key << ", Value: " << (*iter)->Value << endl << endl;
+			(*iter)->Output();
 
-			//출력된 오브젝트의 value값이 일정 값 이상이 된다면 
-			if ((*iter)->Value >= 50)
+			//오브젝트의 value값이 일정 값 이상이 된다면 
+			if ((*iter)->Update())
 			{
 				//현재 리스트에서 DesableList로 옮김
 				DesableList.push_back((*iter));
@@ -81,12 +122,6 @@ int main(void)
 			}
 			else
 				++iter; //증감문
-		}
-
-		cout << "DesableList" << endl;
-		for (list<Object*>::iterator iter = DesableList.begin(); iter != DesableList.end();++iter)
-		{
-			cout << "Key: " << (*iter)->Key << endl;
 		}
 
 		Sleep(50);
@@ -105,4 +140,12 @@ int main(void)
 	EnableList.clear();
 
 	return 0;
+}
+
+void Output(float _x, float _y, string _str)
+{
+	COORD Pos = {_x, _y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+
+	cout << _str << endl;
 }
