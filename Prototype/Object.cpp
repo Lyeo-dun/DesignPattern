20d0ea3e46cb9c05e5b03b3b9da610c5124ca21f
hjd0830 <hjd0830@gmail.com>
2021-08-13 10:07:16 +0900

    Object: 괄호수정

--- Prototype/Object.cpp
+++ Prototype/Object.cpp
@@ -18,7 +18,7 @@ int Object::Update()
 }
 void Object::Render()
 {
-	COORD Pos = { (SHORT)Transinfo.Position.x, (SHORT)Transinfo.Position.y + Transinfo.Index };
+	COORD Pos = { (SHORT)Transinfo.Position.x, SHORT(Transinfo.Position.y + Transinfo.Index) };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 
 	cout << Transinfo.Index << " : " << str << endl;
